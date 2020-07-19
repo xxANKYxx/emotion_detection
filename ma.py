2a0a011636e34e80b20fbc36c80bd1cdf5478626
Ankit Yadav <59208025+xxANKYxx@users.noreply.github.com>
2020-07-19 13:47:35 +0530

    Update ma.py

--- ma.py
+++ ma.py
@@ -18,10 +18,11 @@ while (cap.isOpened()):
         cropped_img=np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
         emotion_prediction = model.predict(cropped_img)
         maxterm = int(np.argmax(emotion_prediction))
-
+        cv2.rectangle(frame,(x,y),(x+w,y+h), color=(0, 251, 255), thickness=2)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, res_dict[maxterm], (15, 25), font, 1, color=(0, 225, 0), thickness=1)
+    cv2.putText(frame,'Press Q to quit', (200, 400), font, 1, color=(255,216, 20), thickness=2)
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
